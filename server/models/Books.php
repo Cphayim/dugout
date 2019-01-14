b74006106ce2b95162df4e2ea1bb9d0bcc6d6f6c
Cphayim <i@cphayim.me>
2019-01-14 18:07:21 +0800

    Add Yii server implementation.

---
+++ server/models/Books.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id ID
+ * @property string $title 书名
+ * @property string $isbn ISBN
+ * @property string $author 作者
+ * @property string $price 价格
+ */
+class Books extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'isbn'], 'required'],
+            [['title'], 'string', 'max' => 30],
+            [['isbn', 'price'], 'string', 'max' => 20],
+            [['author'], 'string', 'max' => 15],
+            [['isbn'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => '书名',
+            'isbn' => 'ISBN',
+            'author' => '作者',
+            'price' => '价格',
+        ];
+    }
+}
---
+++ server/views/books/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\BooksSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = '书籍';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="books-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('创建', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'isbn',
+            'author',
+            'price',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
